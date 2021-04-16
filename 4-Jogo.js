192f63190f572c84527f574ab0ee99874d8dd8e4
Riller <rvsriller@gmail.com>
2021-04-16 03:29:47 -0300

    Desafio 4: Jogo de Pedra, Papel, Tesoura, Lagarto e Spock

--- 4-Jogo.js
+++ 4-Jogo.js
@@ -12,17 +12,16 @@ let  jogador2 = 'marcia';
 
 let resultado;
 
-let N = parseInt(gets());
+let N = parseInt(gets()); //Quantidade de casos a serem testados
 
 // continue a solução
-
 for(let i = 0; i < N; i++){
 
-let escolha = gets().split(' ');
-if( escolha[0]   ===  escolha[1])
-resultado = 'empate';
-else
-resultado = (  dicionarioDeRegras[escolha[0]] .includes(escolha[1])  )? jogador1  :  jogador2;
+    let escolha = gets().split(' '); //Divide as escolhas
+    if( escolha[0]   ===  escolha[1]) //Compara se são iguais
+        resultado = 'empate';
+    else 
+        resultado = (  dicionarioDeRegras[escolha[0]] .includes(escolha[1])  ) ? jogador1  :  jogador2;
 
-print(resultado);
+    print(resultado);
 }
