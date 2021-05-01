59e5a19714317fb8fbfa1b4ff8f024201e40929f
Riller <rvsriller@gmail.com>
2021-04-30 22:41:52 -0300

    Desafio 4: Jogo de Pedra, Papel, Tesoura, Lagarto e Spock

--- 4-Jogo.js
+++ 4-Jogo.js
@@ -1,8 +1,13 @@
 /* 
 
-Pedra-papel-tesoura-lagarto-Spock é uma expansão do clássico método de seleção em jogo de pedra-papel-tesoura. Atua sob o mesmo princípio básico, mas inclui outras duas armas adicionais: o lagarto (formado pela mão igual a uma boca de fantoche) e Spock (formada pela saudação dos vulcanos em Star Trek). Isso reduz as chances de uma rodada terminar em um empate. O jogo foi inventado por Sam Kass e Karen Bryla, como "Rock Paper Scissors Lizard Spock". As regras de vantagem são as seguintes:
+Pedra-papel-tesoura-lagarto-Spock é uma expansão do clássico método de seleção em jogo de pedra-papel-tesoura. 
+Atua sob o mesmo princípio básico, mas inclui outras duas armas adicionais: o lagarto 
+(formado pela mão igual a uma boca de fantoche) e Spock (formada pela saudação dos vulcanos em Star Trek). 
+Isso reduz as chances de uma rodada terminar em um empate. O jogo foi inventado por Sam Kass e Karen Bryla, 
+como "Rock Paper Scissors Lizard Spock". As regras de vantagem são as seguintes:
 
-Haverá diversos casos de teste. O primeiro número a ser lido será um inteiro, representando a quantidade de casos de teste. Cada caso de teste tem duas palavras, representando a escolha de Fernanda e de Marcia, respectivamente.
+Haverá diversos casos de teste. O primeiro número a ser lido será um inteiro, representando a quantidade de casos de teste. 
+Cada caso de teste tem duas palavras, representando a escolha de Fernanda e de Marcia, respectivamente.
 
 Para cada caso de teste, imprima quem venceu, ou se houve empate.
 
@@ -34,5 +39,5 @@ for(let i = 0; i < N; i++){
     else 
         resultado = (  dicionarioDeRegras[escolha[0]] .includes(escolha[1])  ) ? jogador1  :  jogador2;
 
-    print(resultado);
+    print(resultado); //Saída
 }
